53cf0261a094b340bd17795ee67c34b747a201a8
Fred Nixon <fred@johnfnixon.com>
2012-07-09 22:09:09 -0400

    New rails setup for the timestamp app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timestamp::Application.config.secret_token = '8062aa5911e08ddfb52844daaf57b72beac4c8694ca15d8bce408ac6fc18485212ba99950afac2b718433e93a99a41064a52420e8c61370b6c8756b27a930a55'
