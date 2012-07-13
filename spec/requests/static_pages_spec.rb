ead0398c080ff1829fd4f275092d48ec5678955e
Fred Nixon <fred@johnfnixon.com>
2012-07-12 22:43:38 -0400

    Continue modifying layout
    add Contacts page
    make links work in header/footer
    adjust routes to be simpler
    add a root route
    remove the default index page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,39 +1,51 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home GET static_pages_home_path" do   
+  describe "Home GET home_path" do   
     it "should have the title 'Home'" do
-      visit static_pages_home_path
+      visit home_path
       page.should have_selector("title", :text => 'Home')
     end
     
     it "should have the content 'Home'" do
-      visit static_pages_home_path
+      visit home_path
       page.should have_selector('h1', :text => "Welcome to TimeStamp")
     end
   end
   
-  describe "About GET static_pages_about_path" do
+  describe "About GET about_path" do
     it "should have the title 'About'" do
-      visit static_pages_about_path
+      visit about_path
       page.should have_selector("title", :text => 'About')
     end
     
     it "should have the content 'About'" do
-      visit static_pages_about_path
+      visit about_path
       page.should have_selector("h1", :text => 'About')
     end
   end
 
-  describe "Help GET static_pages_help_path" do
+  describe "Help GET help_path" do
     it "should have the title 'Help'" do
-      visit static_pages_help_path
+      visit help_path
       page.should have_selector("title", :text => 'Help')
     end
     
     it "should have the content 'Help'" do
-      visit static_pages_help_path
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
-  end  
+  end
+  
+  describe "Contact GET help_path" do
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector("title", :text => 'Contact')
+    end
+    
+    it "should have the content 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', :text => 'Contact')
+    end
+  end
 end
