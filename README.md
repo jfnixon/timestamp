48549ff5630a2fd74b16391d9dae52a162b0777b
Fred Nixon <fred@johnfnixon.com>
2012-07-24 13:02:55 -0400

    Improve processing of dropped files
    Allow sets of dropped files, via the self-calling anonymous function.
    Add in bootbox modal dialogs.

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 
 **Timestamp** is an application that will permit a user to **Timestamp Files** using a hash function on the local machine. The hash value will be uploaded to the **timestamp server** where an accurate time and a nonce will be added, signed, and returned to the user.
 
+## Requires
+
+- **Twitter Bootstrap** is the framework. https://github.com/twitter/bootstrap/
+- **Bootbox** adds modal dialogs, including javascript alerts.  https://github.com/makeusabrew/bootbox
+
 ## Features
 
 - User accounts
--- app/assets/javascripts/dnd.js
+++ app/assets/javascripts/dnd.js
@@ -6,64 +6,77 @@
 
 /* register the dropzone div, which has id 'drop_area' */
 $(document).ready(function() {
- 
+	
+	// Check for the various File API support.
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+	  // Great success! All the File APIs are supported.
+	} else {
+	  bootbox.alert('The File APIs are not fully supported in this browser.');
+	  return;
+	}
+
   // init event handlers
-  $('#drop_area').on("dragenter", noopHandler);
-  $('#drop_area').on("dragexit",  noopHandler);
-  $('#drop_area').on("dragover",  noopHandler);
+  $('#drop_area').on("dragenter", nobubbleHandler);
+  $('#drop_area').on("dragexit",  nobubbleHandler);
+  $('#drop_area').on("dragover",  nobubbleHandler);
   $('#drop_area').on("drop",      dropHandler);
 });
 
-/* Stop propagation of DnD events */
-function noopHandler(evt) {
+/* Stop bubbling propagation of DnD events */
+function nobubbleHandler(evt) {
   evt.originalEvent.stopPropagation();
   evt.originalEvent.preventDefault();
 }
 
+// show this is a copy event
+function dragoverHandler(evt) {
+	nobubbleHandler(evt);
+	evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
 /* handle the dropped file */
 function dropHandler(evt) {
+  evt.originalEvent.stopPropagation();
+  evt.originalEvent.preventDefault();
 
-	evt.originalEvent.stopPropagation();
-	evt.originalEvent.preventDefault();
-	
   var files = evt.originalEvent.dataTransfer.files;
 	var count = files.length;
 
-	var output = [];
-	// Call the handler for each dropped file.
+	// Issue an async read for each dropped file.
+	// Precess the file when the read completes.
+	// Move the HTML stuff out soon.
 	for (var i = 0; i < count; i++) {
-		//if ($('div#drop_result').is(':visible')) {
-		//        $('div#drop_result').hide();
-		//}
-		//if ($('div#drop_status').is(':hidden')) {
-		//        $('div#drop_status').show();
-		//}    
+		var reader = new FileReader();
 		var f = files[i];
-		output.push('<li><strong>', f.name, '</strong> (', f.type || 'n/a', ') - ', f.size, ' bytes</li>');
-    procFiles(f);
-	}
-	$('#drop_status').html('<ul>' + output.join('') + '</ul>');
-}
-
-/* do the file processing, first, read the file in, then trigger the processing in handleReaderLoad */
-function procFiles(file) {
-
-	// document.getElementById("droplabel").innerHTML = "Processing " + file.name;
-
-	var reader = new FileReader();
-
-	// init the reader event handler (we'll need extra error processing here)
-	reader.onload = handleReaderLoad;
-
-	// begin the read operation, encoding the data as Base64
-	reader.readAsDataURL(file);
-}
-
-/* process the file, when we show up here, the file is Base64 encoded */
-function handleReaderLoad(evt) {
 		
-	hash = hex_sha512(evt.target.result)
-	
-	// create the hash result and update the display
-	$('#drop_result').html('<p><b>Hash: </b>' + hash + '</p>');
+		reader.onerror = errorHandler;
+		reader.onload  = (function(file) { return function(e) {
+			  var hash = hex_sha512(e.target.result);
+			  var output = [];
+			
+			  output.push('<li><strong>', file.name, '</strong> (', file.type || 'n/a', ') - ', file.size, ' bytes</li>');
+			  // create the hash result and update the display
+			  $('#drop_result').append('<p><b>Hash: </b>' + hash + '</p>');			
+			  $('#drop_status').append('<ul>' + output.join('') + '</ul>');
+		  };
+		})(f);
+
+		reader.readAsDataURL(f);
+	}
+}
+
+function errorHandler(evt) {
+	switch(evt.target.error.code) {
+		case evt.target.error.NOT_FOUND_ERR:
+			bootbox.alert('File ' + evt.target.name + ' Not Found!');
+			break;
+		case evt.target.error.NOT_READABLE_ERR:
+			bootbox.alert('File ' + evt.target.name + ' is not readable!');
+			break;
+		case evt.target.error.ABORT_ERR:
+		  bootbox.alert('File ' + evt.target.name + ' read was aborted!')
+			break; // noop
+		default:
+			bootbox.alert('An error occurred reading ' + evt.target.name);
+	};
 }
